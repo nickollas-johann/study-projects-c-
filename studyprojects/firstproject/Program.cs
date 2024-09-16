2067c032d5369a92b5e3c8a3e2689a8a09ff5756
Nickollas <nickjohalb@outlook.com>
2024-09-15 21:58:45 -0300

    :sparkles: first commit

---
+++ studyprojects/firstproject/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace firstproject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                try { 
+                
+                Console.Write("Digite o primeiro número: ");
+                string n1 = Console.ReadLine();
+                int number1 = int.Parse(n1);
+
+                Console.Write("Digite o segundo número: ");
+                string n2 = Console.ReadLine();
+                int number2 = int.Parse(n2);
+
+                Console.Write("Digite o operador um dos operadores +,-,*,/: ");
+                string operador = Console.ReadLine();
+                int result = 0;
+
+                switch (operador)
+                {
+                    case "+":
+                        result = number1 + number2;
+                        break;
+                    case "-":
+                        result = number1 - number2;
+                        break;
+                    case "*":
+                        result = number1 * number2;
+                        break;
+                    case "/":
+                        result = number1 / number2;
+                        break;
+                    default:
+                        Console.WriteLine("Operador inválido");
+                        break;
+
+                }
+                Console.WriteLine($"O resultado é: {result}");
+                } catch {
+                    Console.WriteLine("Desculpe, parace que você digitou um valor errado");
+
+                }
+
+
+
+
+
+
+            }
+        }
+    }
+}
